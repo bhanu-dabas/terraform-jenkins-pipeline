5499bff48e32e649031f5aad565994c7aca6c8c6
bhanu-dabas <160500397+bhanu-dabas@users.noreply.github.com>
2024-05-24 18:27:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,3 @@ resource "aws_instance" "public_instance" {
    Name = var.name_tag,
  }
 }
-resource "aws_instance" "private_instance" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_pair
-
-  tags = {
-    Name = var.name_tag
-  }
-}
